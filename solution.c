c180efde89f13d830428cb886d43dae53228a938
max0604 <akimov0604@yandex.ru>
2016-06-27 15:37:16 +0300

    Add files via upload

--- solution.c
+++ solution.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <mqueue.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <string.h>
-#include <malloc.h>
+
+const int semaphore_nb = 16;
 
 int main()
 {
-	//mqd_t mq_open(const char *name, int oflag, mode_t mode, struct mq_attr *attr);
-	//int mq_send(mqd_t mqdes, const char *msg_ptr, size_t msg_len, unsigned int msg_prio);
-	//ssize_t mq_receive(mqd_t mqdes, char *msg_ptr, size_t msg_len, unsigned int *msg_prio);
-	struct mq_attr attr;
-	attr.mq_maxmsg = 5;
-	attr.mq_msgsize = 256;
-	mqd_t q_id = mq_open("/test.mq", O_CREAT | O_RDONLY, 0664, &attr );
-	if( -1 == q_id ) {
-		perror("mq_open");
-	}
+	key_t key = ftok("/tmp/sem.temp", 1);
 
-	long buffer_size = attr.mq_msgsize + 1;
-	char* memory = malloc( buffer_size );
-	memset(memory, 0, buffer_size);
-	ssize_t nb =  mq_receive(q_id, memory, buffer_size - 1, NULL);
-
-	if( nb > 0 ) {
-		FILE* file = fopen("/home/box/message.txt", "w+");
-		fprintf(file, "%s", &memory[0]);
-		fclose(file);
-	} else {
-		perror("mq_receive");
+	int sem_set_id = semget(key, semaphore_nb, IPC_CREAT);
+	if( -1 == sem_set_id ) {
+		perror("semget");
 	}
-	free(memory);
-	mq_close(q_id);
-	mq_unlink("/test.mq");
+	
+	unsigned short args[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+	int res = semctl( sem_set_id, 0, SETALL, args );
+	
+	
+	res = semctl(sem_set_id, 0, IPC_RMID, 0);
+	if( -1 == res ) {
+		perror("semctl IPC_RMID");
+	}
 	return 0;
 }
